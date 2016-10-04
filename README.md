a799c509e7216f8905274c14df752a211f82135d
Tim <sportingapp+heroku@gmail.com>
2016-10-04 11:56:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# organize pictures to date folder using Exif data 
+
+## How to Use
+1. execute ezOrganizePic.sh and give one argument to assign the pictures folder
+
+## Result
+1. copy all pictures to backup folder
+2. all pictures will move to their date folder
---
+++ ezOrganizePic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# check input parameters exists
+if [ -z "$1" ]; then
+  printf 'please enter 1 input arguments: pic folder';
+  exit;
+fi
+
+searchFolder=$1
+jpgList=$searchFolder'/jpg.lst'
+folderList=$searchFolder'/folder.lst'
+mvList=$searchFolder'/mv.lst'
+backupFolder=$searchFolder'/backup'
+
+rm $jpgList
+rm $folderList
+rm $mvList
+
+find $searchFolder -maxdepth 1 -type f -name '*.JPG' >> $jpgList
+find $searchFolder -maxdepth 1 -type f -name '*.jpg' >> $jpgList
+
+#extract jpg filename to fullfilename@filename@datetime
+cat $jpgList | while read x;do echo $x"@""${x##*/}""@"$(jhead $x |grep  Date/Time| cut -c -25 |awk -F ':' '{print $2$3$4}'|tr -d [:blank:]) >> $folderList; done
+
+#mkdir
+awk -F '@' '{print $3}' $folderList |sed '/^$/d' | while read x;do mkdir $x;done 
+
+#clone jpg files to backup folder
+mkdir $backupFolder
+awk -F '@' '{print ($3 != "")? $1"@"$2"@"$3 : ""}' $folderList|sed '/^$/d' |while read x; do cp $(echo $x|awk -F '@' '{print $1}') $backupFolder/$(echo $x|awk -F '@' '{print $2}'); done
+
+#mv jpg files to date folder
+awk -F '@' '{print ($3 != "")? $1"@"$2"@"$3 : ""}' $folderList|sed '/^$/d' |while read x; do mv $(echo $x|awk -F '@' '{print $1}') $(echo $x|awk -F '@' '{print $3"/"$2}'); done
+
+
